1cd0e822e3cad1d9aea9e411f050431857779ce7
dimanter <dmitriy_makhov@bk.ru>
2019-05-23 16:07:36 +0700

    Update code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,55 @@
 #include <clocale>
 using namespace std;
 
+int ProverNik(char p[15])
+{
+	int i=0,t=0;
+	while(i<15)
+	{
+		if(p[i]==0)
+		{
+			break;
+		}
+		if(p[i]=='a' || p[i]=='b' || p[i]=='c' || p[i]=='d' || p[i]=='e' || p[i]=='f' || p[i]=='g' || p[i]=='h' || p[i]=='j' || p[i]=='k' || p[i]=='l' || p[i]=='m' || p[i]=='n' || p[i]=='o')
+		{
+			
+		}
+		else
+		{
+			if(p[i]=='p' || p[i]=='r' || p[i]=='s' || p[i]=='t' || p[i]=='u' || p[i]=='v' || p[i]=='w' || p[i]=='u' || p[i]=='y' || p[i]=='z' || p[i]=='x' || p[i]=='q')
+			{
+				
+			}
+			else
+			{
+				if(p[i]=='A' || p[i]=='B' || p[i]=='C' || p[i]=='D' || p[i]=='E' || p[i]=='F' || p[i]=='G' || p[i]=='H' || p[i]=='J' || p[i]=='K' || p[i]=='L' || p[i]=='M' || p[i]=='N' || p[i]=='O')
+				{
+					
+				}
+				else
+				{
+					if(p[i]=='P' || p[i]=='R' || p[i]=='S' || p[i]=='T' || p[i]=='U' || p[i]=='V' || p[i]=='W' || p[i]=='U' || p[i]=='Y' || p[i]=='Z' || p[i]=='X' || p[i]=='Q')
+					{
+						
+					}
+					else
+					{
+						t++;
+					}
+				}
+			}
+		}
+		i++;
+	}
+	if(t==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
 
 void Data(char p[15],int k,int n,int Spich)
 {

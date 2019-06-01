dd9e32014533e7f9a6cabf354d30efb692c82ae0
dimanter <dmitriy_makhov@bk.ru>
2019-06-01 13:34:10 +0700

    Pizosss

--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,20 @@
 #include "Case.h"
 #include "catch.hpp"
 
-TEST_CASE("TEST", "NICKNAME")
+TEST_CASE("TEST", "HOD")
 {
     REQUIRE(Prover(9, 100) == 0);
+    REQUIRE(Prover(11, 90) == 1);
+    REQUIRE(Prover(9, 7) == 1);
+}
+TEST_CASE("Tot", "NICKNAME")
+{
+    char p[15] = {'a'};
+    REQUIRE(ProverNik(p) == 0);
+    p[1] = 1;
+    p[2] = '2';
+    p[4] = '/';
+    p[13] = '_';
+    p[14] = '+';
+    REQUIRE(ProverNik(p) == 1);
 }
